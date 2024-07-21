1164ff86f10c92d45b99c21c0ac1837fa8407365
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-07-21 16:21:54 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from "./components/ui/button"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Button>hi world</Button>
+    </>
+  )
+}
+
+export default App
