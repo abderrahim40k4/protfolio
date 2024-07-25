3c52089f2ba4a8dee123f48018e6155183280cca
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-07-25 16:26:17 +0200

    them addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme-provider"
+import {ModeToggle} from "@/components/mode-toggle"
 
 function App() {
 
   return (
     <>
-      <div>
-        <Button>test</Button>
-       </div>
+       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+           <ModeToggle>dark</ModeToggle>
+      </ThemeProvider>
     </>
   )
 }
