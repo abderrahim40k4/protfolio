b98543e0b109b697201b02c9c5d7c96e75e2cf8d
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-07-25 15:55:34 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button>test</Button>
+       </div>
+    </>
+  )
+}
+
+export default App
