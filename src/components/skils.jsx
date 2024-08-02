d7018f5df9aac9e1e318bb7114901af176c9fbd5
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-08-02 21:04:36 +0200

    Update Skils

--- src/components/skils.jsx
+++ src/components/skils.jsx
@@ -27,19 +27,93 @@ export function Skils() {
               <TableRow>
                 <TableCell className="items-start">HTML</TableCell>
                 <TableCell className="font-medium">
-                <TableCell className="font-medium">HTML</TableCell><br></br>
-                <TableCell className="font-medium">HTML5</TableCell>
+                  HTML5/XHTML
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={95}/>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="items-start">CSS</TableCell>
+                <TableCell className="font-medium">
+                  CSS3/SCSS
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={92}/>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="items-start">CSS Frameworks</TableCell>
+                <TableCell className="font-medium">
+                  TailwindCss/Bootstrup
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={90}/>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="items-start">Javascript</TableCell>
+                <TableCell className="font-medium">
+                  Vanila/ESC6/Tyapescript
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={89}/>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="items-start">PHP</TableCell>
+                <TableCell className="font-medium">
+                  PHP/LARAVEL
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={80}/>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="items-start">Javascript Framwworks</TableCell>
+                <TableCell className="font-medium">
+                  React/vue/node
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={90}/>
                 </TableCell>
 
+              </TableRow>
+              <TableRow>
+                <TableCell className="items-start">SQL</TableCell>
                 <TableCell className="font-medium">
-                <TableCell className="font-medium w-1/3">
-                  <Progress value={33} className="w-full"/> test
-                </TableCell><br/>
-                <TableCell className="font-medium w-1/3">
-                  <Progress value={33} className="w-full"/> test
+                  mySQL/SQLServer 
                 </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={76}/>
                 </TableCell>
-    
+              </TableRow>
+              <TableRow>
+                <TableCell className="items-start">NoSQL</TableCell>
+                <TableCell className="font-medium">
+                  MongeDB
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={60}/>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="items-start">control version </TableCell>
+                <TableCell className="font-medium">
+                  Git/Githup
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={95}/>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="items-start">Bonus</TableCell>
+                <TableCell className="font-medium">
+                  Python/Doker
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Progress value={78}/>
+                </TableCell>
               </TableRow>
             </TableBody>
           </Table>
