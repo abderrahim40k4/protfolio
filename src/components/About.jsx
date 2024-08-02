95d3039bb55b9a040911c4e8e26b9c1114d0b8db
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-08-02 21:14:30 +0200

    responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,8 @@ export function About() {
     return (
       <>
           <div className="w-full flex flex-row justify-evenly items-center p-10 rounded-xl text-2xl font-semibold">
-            <Button className="w-5/12 md:p-8 p-4 text-lg md:text-xl bg-blue/100">Download CV <ArrowDownToDot size={26} className="mx-4" /></Button>
-            <Button className="w-5/12 md:p-8 p-4 text-lg md:text-xl bg-primary-foreground text-primary hover:text-secondary hover:bg-secondary-foreground">Contact me</Button>
+            <Button className="w-5/12 md:p-8 p-2 text-sm md:text-xl bg-blue/100">Download CV <ArrowDownToDot size={26} className="mx-4" /></Button>
+            <Button className="w-5/12 md:p-8 p-2 text-sm md:text-xl bg-primary-foreground text-primary hover:text-secondary hover:bg-secondary-foreground">Contact me</Button>
           </div>
       </>
     )
--- src/components/skils.jsx
+++ src/components/skils.jsx
@@ -26,92 +26,92 @@ export function Skils() {
             <TableBody>
               <TableRow>
                 <TableCell className="items-start">HTML</TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   HTML5/XHTML
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={95}/>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell className="items-start">CSS</TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   CSS3/SCSS
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={92}/>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell className="items-start">CSS Frameworks</TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   TailwindCss/Bootstrup
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={90}/>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell className="items-start">Javascript</TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   Vanila/ESC6/Tyapescript
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={89}/>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell className="items-start">PHP</TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   PHP/LARAVEL
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={80}/>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell className="items-start">Javascript Framwworks</TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   React/vue/node
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={90}/>
                 </TableCell>
 
               </TableRow>
               <TableRow>
                 <TableCell className="items-start">SQL</TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   mySQL/SQLServer 
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={76}/>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell className="items-start">NoSQL</TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   MongeDB
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={60}/>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell className="items-start">control version </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   Git/Githup
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={95}/>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell className="items-start">Bonus</TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   Python/Doker
                 </TableCell>
-                <TableCell className="font-medium">
+                <TableCell className="md:font-meduim font-thin">
                   <Progress value={78}/>
                 </TableCell>
               </TableRow>
