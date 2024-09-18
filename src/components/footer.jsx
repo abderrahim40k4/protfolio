361bf97af5fcc33887c36697c7b47f8731777cb1
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-18 10:07:32 +0200

    footer updates

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,8 @@ export function Footer() {
 
     return (
       <>
-          <div className="w-full flex  justify-center items-center p-4 text-center">
-          © Abderrahim Ait oufassi. 2024 All rigths reserved
+          <div className="w-full flex  justify-center items-center p-4 text-center font-medium">
+          ©Abderrahim Ait oufassi. 2024 All rigths reserved
           </div>
       </>
     )
--- src/components/theme-provider.jsx
+++ src/components/theme-provider.jsx
@@ -21,7 +21,7 @@ export function ThemeProvider({
   useEffect(() => {
     const root = window.document.documentElement
 
-    root.classList.remove("light", "dark")
+    root.classList.remove("light", "dark");
 
     if (theme === "system") {
       const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
