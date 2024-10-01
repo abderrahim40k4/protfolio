cf10a6e95f8fe75569b96f89741eb65ede2b89e4
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-10-01 15:36:40 +0200

    update

--- src/components/Exprience.jsx
+++ src/components/Exprience.jsx
@@ -8,9 +8,9 @@ const Exprience = () => {
   
   {/* Neon rotating border */}
   
-  <div className="relative">
+  <div className="relative flex flex-col w-full justify-between items-center">
     {EXPERIENCES.map((experience, index) => (
-      <div className='flex flex-wrap items-center lg:justify-center mb-4 clip' key={index}> 
+      <div className='flex flex-wrap items-center lg:justify-center mb-4 clip bg-primary-foreground md:w-4/5 w-full p-2' key={index}> 
         <div className="w-full">
           <p className='mb-4 text-neutral-400 text-sm'>{experience.year}</p>
         </div>
@@ -26,7 +26,7 @@ const Exprience = () => {
           </p>
           {
             experience.technologies.map((tech, index) => (
-              <Button default key={index} className='mr-2 mt-4 rounded py-1 px-2 text-xs lg:text-sm font-medium text-blue mb-2 bg-primary-foreground hover:text-secondary hover:bg-secondary-foreground'>
+              <Button default key={index} className='mr-2 mt-4 rounded py-1 px-2 text-xs lg:text-sm font-medium text-blue mb-2 bg-secondary hover:text-secondary hover:bg-secondary-foreground'>
                 {tech}
               </Button>
             ))
