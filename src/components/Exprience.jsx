8b65d9ac5eeb2bcb7f98c75ee2c928cff4548e24
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-10-02 13:35:37 +0200

    update

--- src/components/Exprience.jsx
+++ src/components/Exprience.jsx
@@ -6,8 +6,6 @@ const Exprience = () => {
    <div className='relative border-b border-neutral-900 pb-4'>
   <h2 className="my-20 text-4xl text-center">Work History</h2>
   
-  {/* Neon rotating border */}
-  
   <div className="relative flex flex-col w-full justify-between items-center">
     {EXPERIENCES.map((experience, index) => (
       <div className='flex flex-wrap items-center lg:justify-center mb-4 clip bg-primary-foreground md:w-4/5 w-full p-2' key={index}> 
