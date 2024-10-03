a1417535d3979f366d3782eb734554714f2cac3a
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-10-03 17:42:00 +0200

    update ui

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -9,16 +9,20 @@ export function Tab() {
       <div className="w-full flex flex-row justify-evenly items-center p-5">
         <Tabs defaultValue="Portfolio" className="w-full py-4">
           <TabsList>
-            <TabsTrigger value="Portfolio" className="clip">Portfolio</TabsTrigger>
-            <TabsTrigger value="Skills" className="clip">Skills</TabsTrigger>
-            <TabsTrigger value="Exprience" className="clip">Exprience</TabsTrigger>
+            <TabsTrigger value="Portfolio" className="clip py-2 md:py-4">Portfolio</TabsTrigger>
+            <TabsTrigger value="Skills" className="clip py-2 md:py-4">Skills</TabsTrigger>
+            <TabsTrigger value="Exprience" className="clip py-2 md:py-4">Exprience</TabsTrigger>
           </TabsList>
           <TabsContent value="Portfolio">
             <Portfolio />
           </TabsContent>
 
-          <TabsContent value="Skills"> <Skils /> </TabsContent>
-          <TabsContent value="Exprience"> <Exprience /></TabsContent>
+          <TabsContent value="Skills">
+            <Skils />
+          </TabsContent>
+          <TabsContent value="Exprience">
+            <Exprience />
+          </TabsContent>
 
         </Tabs>
       </div>
